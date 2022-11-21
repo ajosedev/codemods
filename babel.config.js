5d607c85a254e9146a348023bb2d6279885e5938
Andrew Jose <ajosedev@gmail.com>
2022-11-21 19:44:40 +1100

    Add react

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-typescript'],
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-typescript',
+        [
+            '@babel/preset-react',
+            {
+                runtime: 'automatic',
+            },
+        ],
+    ],
 };
