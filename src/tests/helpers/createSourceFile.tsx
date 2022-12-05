2a3076860ac1fbcd8c03aae59fb837c92eab015e
Andrew Jose <ajosedev@gmail.com>
2022-12-05 17:06:26 +1100

    Set manipulationSettings quoteKind

--- src/tests/helpers/createSourceFile.tsx
+++ src/tests/helpers/createSourceFile.tsx
@@ -1,9 +1,14 @@
 import { stripIndent } from 'common-tags';
-import { Project } from 'ts-morph';
+import { Project, QuoteKind } from 'ts-morph';
 
 // Helper function to create a source file in a fake ts-morph project
 export function createSourceFile(source: string) {
-    const project = new Project({});
+    const project = new Project({
+        manipulationSettings: {
+            quoteKind: QuoteKind.Single,
+        },
+    });
+
     return project.createSourceFile(
         // TSX to support JSX
         'test.tsx',
